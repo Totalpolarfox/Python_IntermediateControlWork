8743320293a9be272c951de656ab0327826a2108
Totalpolarfox <totalpolarfox@outlook.com>
2024-04-15 09:55:08 +0300

    Added a data output function and a function for reading data from a file

--- main.py
+++ main.py
@@ -4,6 +4,25 @@ def create_file_csv(file: str = 'file name'):
         f.write('ID;Заголовок;Заметка;Дата_создания\n')
     print(f'\u001b[32mФайл {file} создан\u001b[0m')
 
+# функция вывода данных
+def show_data(data: list[str]):
+    for element in data:
+        print(f'ID: {element[0]}  Дата создания: {element[3]}')
+        print(f'\tЗаголовок: {element[1]}')
+        print(f'\tЗаметка: {element[2]}')
+    # print(data)
+
+# функция чтения данных из файла
+def read_data(file: str = 'file name'):
+    try:
+        with open(file, 'r', encoding='utf-8') as f:
+            csv_reader = csv.reader(f, delimiter = ";")
+            return list(csv_reader)[1:] # удаляет строку заголовка
+
+    except FileNotFoundError:
+        print('\u001b[31mФайл  не найден.\n\u001b[0m')
+        return []  
+
 def main():
     file_name = input('Введите имя файла, в котором будут хранится заметки: ') + '.csv'
     create_file_csv(file_name)
@@ -18,7 +37,15 @@ def main():
         print('\u001b[32m 5 \u001b[0m - Удалить заметку')
         print()
         answer = input('\u001b[4mВыберите необходимое действие:\u001b[0m ')
-
+        if answer == '0':
+            os.system('cls')
+            print('Выбрано: \u001b[32mВыход из программы \u001b[0m')
+            flag = False
+        elif answer == '1':
+            print('Выбрано: \u001b[32mПросмотреть все заметки \u001b[0m')
+            data = read_data(file_name)
+            show_data(data)        
+    
 
 if __name__ == '__main__':
     main()
